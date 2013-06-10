04dc6df28ddac765c4f196a2eb69290c97b0cd6e
Bacra Woo <Bacra.Woo@gmail.com>
2013-06-10 18:17:56 +0800

    publish

---
+++ Emitter.js
@@ -0,0 +1,243 @@
+var Emitter = (function(){
+
+	// 全局工具函数
+	function toArray(args){
+		return Array.prototype.slice.call(args);
+	}
+	function isPlainObject(obj){
+		return obj && obj !== null && typeof(obj) == 'object';
+	}
+	function getParamFunc(getParam, setParam, obj){
+		return function(data, widthBaseParam){
+			if (isPlainObject(data)) {
+				setParam(data, widthBaseParam);
+				return obj;
+			} else {
+				return getParam(data, widthBaseParam);
+			}
+		};
+	}
+	function getRunParamFunc(run, base, paramFunc){
+		extendDeep(paramFunc, base);
+		extend(paramFunc, run);
+		return paramFunc;
+	}
+	function extend(obj, obj2) {
+		for (var i  in obj2) {
+			obj[i] = obj2[i];
+		}
+	}
+	function extendDeep(obj, obj2) {
+		var temp, temp2;
+		for (var i in obj2) {
+			temp = obj2[i];
+			if (isPlainObject(temp)) {
+				temp2 = obj[i];
+				if (!temp2) obj[i] = temp2 = {};
+				extendDeep(temp2, obj[i]);
+			} else {
+				obj[i] = temp;
+			}
+		}
+	}
+	function forEach(arr, callback){
+		for(var i = 0, num = arr.length; i < num; i++) {
+			if (callback(arr[i], i) === false) return;
+		}
+	}
+
+	function setDisabledCall(funcData){
+		funcData.disabled = true;
+	}
+
+	function findFuncInList(func, list, callback){
+		forEach(list, function(funcData){
+			if (funcData.func === func) callback(funcData);
+		});
+	}
+	
+
+
+	return function(defaultCall, _obj){
+		var _before = [],
+			_after = [],
+			_final = [],
+			_baseParam = {},
+			_runParam = {};
+		
+
+		function getList(stepName) {
+			return stepName == 'after' ? _after : stepName == 'final' ? _final : _before;	
+		}
+		function addEventListener(stepName, data, func){
+			if (!func) {
+				func = data;
+				data = {};
+			}
+			if (!func) {
+				func = stepName;
+				stepName = '';
+			}
+
+			var funcData = {
+				data: data,
+				func: func,
+				off: function(){
+					funcData.disabled = true;
+				}
+			};
+
+			getList(stepName).push(funcData);
+
+			return funcData;
+		}
+		function setParam(data, widthBaseParam){
+			if (widthBaseParam) extend(_baseParam, data);
+			extend(_runParam, data);
+		}
+		function getParam(name, isBaseParam){
+			return isBaseParam ? _baseParam[name] : _runParam[name];
+		}
+		function removeParam(name, widthBaseParam) {
+			if (widthBaseParam) delete _baseParam[name];
+			delete _runParam[name];
+		}
+		
+		
+
+		return {
+			'on': function(){
+				addEventListener.apply(null, arguments);
+				return _obj;
+			},
+			'off': function(stepName, func){
+				if (stepName && func) {
+					findFuncInList(func, getList(stepName), setDisabledCall);
+				} else if (stepName) {
+					if (typeof(stepName) == 'function') {
+						findFuncInList(stepName, _before, setDisabledCall);
+						findFuncInList(stepName, _after, setDisabledCall);
+						findFuncInList(stepName, _final, setDisabledCall);
+					} else {
+						forEach(getList(stepName), setDisabledCall);
+					}
+				} else {
+					forEach(_before, setDisabledCall);
+					forEach(_after, setDisabledCall);
+					forEach(_final, setDisabledCall);
+				}
+
+				return _obj;
+			},
+			'once': function(){
+				var funcData = addEventListener.apply(null, arguments);
+				addEventListener('final', function(){
+					funcData.disabled = true;
+				});
+			},
+			'param': getParamFunc(getParam, setParam, _obj),
+			'removeParam': removeParam,
+			'trigger': function(){
+				if (defaultCall) return defaultCall.apply(_obj, arguments);
+			},
+			'emit': function(){
+				var args = toArray(arguments),
+					preReturn, defaultReturn,
+					i, funcData,
+
+					isStop = false,
+					isDefaultPrevented = false,
+					preventDefaultFn = function(){
+						isDefaultPrevented = true;
+					},
+					getAvailableFuncData = function(list, index){
+						var funcData = list[index];
+						if (!funcData) return false;
+
+						if (funcData.disabled) {
+							list.splice(index, 1);
+							return getAvailableFuncData(list, index);
+						}
+						return funcData;
+					},
+					runList = function(list){
+						// before list run
+						for (i = 0; !isStop && i < list.length; i++) {
+							funcData = list[i];
+							if (funcData.disabled) {
+								list.splice(i, 1);
+								i--;
+								continue;
+							}
+
+							args[0] = new Event(funcData);
+
+							preReturn = funcData.func.apply(_obj, args);
+							if (preReturn === false) {
+								isStop = true;
+								break;
+							}
+						}
+					},
+					myRunParam = getRunParamFunc(_runParam, _baseParam, getParamFunc(getParam, setParam, _obj));
+
+				_runParam = {};			// 清空 防止影响到内部的嵌套调用
+
+				var Event = function(funcData){
+					this['data'] = funcData.data;
+					this['off'] = funcData.off;
+					this['preReturn'] = preReturn;
+					this['next'] = function(){					// 调用next只可能返回两种值 true 和 false
+						if (isStop) return false;
+
+						var funcData = getAvailableFuncData(_before, ++i);
+						if (funcData) {
+							args[0] = new Event(funcData);
+							preReturn = funcData.func.apply(_obj, args);
+							if (preReturn === false) {
+								isStop = true;
+								return false;
+							}
+						}
+
+						return true;
+					};
+				};
+				Event.prototype = {
+					'param': myRunParam,
+					'removeParam': removeParam,
+					'preventDefault': preventDefaultFn
+				};
+
+				args.unshift(null);			// Event placeholder
+
+
+				// after list run
+				runList(_before);
+
+				// defaultCall run
+				if (!isStop && !isDefaultPrevented && defaultCall) {
+					args[0] = {
+						'preReturn': preReturn,
+						'param': myRunParam,
+						'removeParam': removeParam
+					};
+					preReturn = defaultReturn = defaultCall.apply(_obj, args);
+					if (preReturn === false) isStop = true;
+				}
+
+
+				Event.prototype['defaultReturn'] = defaultReturn;
+
+				// after list run
+				runList(_after);
+
+				// final list run
+				isStop = false;		// 为final 重置isStop
+				runList(_final);
+
+				return defaultReturn;
+			}
+		};
+	};
+})();
--- README.md
+++ README.md
@@ -1,4 +1,89 @@
 Emitter
 =======
 
-Javascript event listener class with Timeline
+简单快速为JS方法创建监听队列，同时增加时间轴概念，提供`before` `after` `final`三个时间段
+
+
+
+
+Get Started
+-----------
+
+	Emitter(method, obj);
+
+返回对象包含如下方法：
+
+`on([timeline, ] [data, ] func)`: 增加监听事件
+
+`off([timeline, ] [func])`: 删除监听事件（全部/队列全部/指定事件）
+
+`once([timeline, ] [data, ] func)`: 添加临时监听事件
+（注意：不管调用emit后是否运行此方法，都会在emit调用结束后，删除此绑定）
+
+`emit([args])`: 运行队列，返回**method**运行结果（**method**未运行则返回 _undefined_ ）
+
+`param(data [, widthBaseParam])`: 添加/读取运行时所需要的参数
+
+`removeParam(name, [, widthBaseParam])`: 移除指定的参数
+
+`trigger([args])`: 运行默认**method**方法
+
+
+
+
+About Event Class
+-----------------
+
+使用`on`方法添加监听事件，以及使用`Emitter`初始化对象时，所添加函数的第一个参数将强制规定为Event对象
+
+Event对象包含如下属性和方法：
+
+`data`: 执行`on`方法时，传入的`data`对象
+
+`preReturn`: 队列执行过程中，上一个方法的返回值（仅作参考）
+
+`defaultReturn`: 队列执行过程中，默认方法`method`方法的返回值
+
+`off()`: 执行后，将从队列中删除此方法
+
+`next()`: 直接调用队列的下一个方法（注意：不跨队列，并且返回值只有`true`和`false`）
+
+`param(data [, widthBaseParam])`: 添加/读取运行时所需要的参数(注：如果单单只是修改**runParam**，可直接赋值)
+
+`removeParam(name, [, widthBaseParam])`: 移除指定的参数
+
+`preventDefault()`: 运行后，将阻止默认方法`method`的执行
+
+
+
+
+
+About timeline Param
+------------------------
+
+`timeline`有三个值`before`（默认）、 `after`、 `final`：
+
+`before`: 在**method**之前运行；
+
+`after`: 在**method**之后运行（注：使用`event.preventDefault()`方法取消`after`队列的执行；直接`return false;`则可以）
+
+`final`: 在`after`之后运行，即使其他队列（或则默认方法）执行`event.preventDefault()`或则`return false;`，`final`队列也会执行（`final`队列中执行`return false;`可中止）
+
+
+
+
+About param Method
+---------------------
+
+当`data`参数类型为`Object`时，执行set方法；`String`则执行get方法
+
+当`widthBaseParam`定义为`true`时（默认为`false`），set/get的内容为**baseParam**，否则为**runParam**；
+两者的区别是**baseParam**中的值会一直存在，不受到**runParam**的修改而变化；**runParam**中设置的值，在调用一次`emit`后会自动清空（嵌套调用不继承）
+
+
+
+
+License
+-------
+
+Emitter.js is available under the terms of the [MIT License](./LICENSE.md).
