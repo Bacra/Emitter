aa98a4c5b1fa55d1429aac73ff096a6f4ef2923f
Bacra Woo <Bacra.Woo@gmail.com>
2013-06-11 16:55:11 +0800

    fix source map file; add version history file

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,6 @@ module.exports = function(grunt) {
 				src: 'dist/tEmitter.js',
 				dest: 'dist/tEmitter.min.js',
 				options: {
-					banner: '/*! tEmitter <%= pkg.version %> */',
 					source_map_format: 'V3',
 					create_source_map: 'dist/tEmitter.js.map'
 				}
@@ -38,8 +37,36 @@ module.exports = function(grunt) {
 	});
 
 
+	grunt.registerTask('fix', 'fix source map prototype.', function(){
+		var mapfile = "dist/tEmitter.js.map";
+
+		var code = grunt.file.read(mapfile);
+		code = code.replace(/^"sources":\[[^\]]*?\],$/m, '"sources":["tEmitter.js"],');
+		grunt.file.write(mapfile, code);
+
+		var minfile = "dist/tEmitter.min.js",
+			version = grunt.config('pkg.version');
+
+		code = grunt.file.read(minfile);
+		code = '/*! tEmitter '+version+' | MIT*/'+code +grunt.util.linefeed+'//@ sourceMappingURL=tEmitter.js.map';
+
+		grunt.file.write(minfile, code);
+
+	});
+
+
+
+	grunt.registerTask('backup', 'copy backup.', function() {
+		var version = grunt.config('pkg.version');
+		grunt.file.copy('lib/tEmitter.js', 'lib/tEmitter-'+version+'.js');
+		grunt.file.copy('dist/tEmitter.js', 'dist/'+version+'/tEmitter.js');
+		grunt.file.copy('dist/tEmitter.min.js', 'dist/'+version+'/tEmitter.min.js');
+		grunt.file.copy('dist/tEmitter.js.map', 'dist/'+version+'/tEmitter.js.map');
+	});
+
+
 	grunt.loadNpmTasks('grunt-gcc');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 
-	grunt.registerTask('default', ['concat', 'gcc']);
+	grunt.registerTask('default', ['concat', 'gcc', 'fix', 'backup']);
 };
